332e5e353391a5b1a7017d0fa25314e93aec0f38
ricky <erikikinyamagoha@gmail.com>
2026-08-29 23:24:27 +0300

    pdf

--- resources/views/almanac/pdf/almanac.blade.php
+++ resources/views/almanac/pdf/almanac.blade.php
@@ -72,15 +72,11 @@
     tr { page-break-inside: avoid !important; }
 
     .month {
-        width: 34px;
+        width: 22px;
         text-align: center;
-        vertical-align: middle;
         font-weight: normal;
         font-size: 6pt;
         color: #444;
-        word-wrap: break-word;
-        line-height: 1.3;
-        writing-mode: horizontal-tb; /* force normal left-to-right text, never rotated */
     }
     /* bold purple rule on the first day of a new month = strong, unmistakable separator */
     .month-start td, .month-start th {
@@ -150,9 +146,7 @@
             @foreach ($calendar['months'] as $month)
                 @foreach ($month['days'] as $dayIndex => $day)
                     <tr class="{{ $dayIndex === 0 ? 'month-start' : '' }}">
-                        @if ($dayIndex === 0)
-                            <td class="month" rowspan="{{ count($month['days']) }}">{{ $month['label'] }}</td>
-                        @endif
+                        <td class="month">{{ $dayIndex === 0 ? $month['label'] : '' }}</td>
 
                         @foreach ($calendar['groups'] as $index => $group)
                             @php
